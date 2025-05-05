3936d6cf34d127acf398b3db09d827ab564be823
yugantmohan <yugantmohan@gmail.com>
2025-05-05 00:16:10 -0400

    fixed bcrypt library build error

--- todo-backend/src/controllers/authController.ts
+++ todo-backend/src/controllers/authController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 import { generateAccessToken, generateRefreshToken } from '../utils/jwt.js';
