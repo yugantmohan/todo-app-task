d72c3e2fd2a704966d6bfe51100e9e60b0976be1
yugantmohan <yugantmohan@gmail.com>
2025-05-05 00:39:18 -0400

    handled cors

--- todo-backend/src/index.ts
+++ todo-backend/src/index.ts
@@ -11,12 +11,7 @@ import { RequestHandler } from 'express';
 dotenv.config();
 const PORT = process.env.PORT || 3000
 const app = express()
-app.use(
-  cors({
-    origin: 'http://localhost:8080',
-    credentials: true,
-  })
-);
+app.use(cors({origin: true, credentials:true}))
 app.use(express.json());
 app.use(cookieParser());
 
